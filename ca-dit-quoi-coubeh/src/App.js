1131adf177006bf0b7e4953d6f2f11383e197ac8
cityhand <vuilleminethan@gmail.com>
2024-10-19 17:05:24 +0200

    Creation page home
    Dissocier quizz de home dans l'app

--- ca-dit-quoi-coubeh/src/App.js
+++ ca-dit-quoi-coubeh/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import QuizPage from './pages/QuizPage';
+import Home from './pages/Home';
 import './App.css'; // Import global styles
 
 function App() {
@@ -8,7 +9,8 @@ function App() {
     <Router>
       <div className="app-container">
         <Routes>
-          <Route path="/" element={<QuizPage />} />
+        <Route path="/" element={<Home />} />
+          <Route path="/quizz" element={<QuizPage />} />
         </Routes>
       </div>
     </Router>
